2f8d7279841bb05b93915b2734c29faf5aa400dc
Zathras <ChampionMike42@gmail.com>
2021-07-05 21:54:06 -0500

    Add new item to questions array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-// const generate = import("./utils/generateMarkdown.js");
-const generate = require("./utils/generateMarkdown.js");
+const generate = require("./utils/generateMarkdown");
 const fs = require("fs");
+const path = require("path");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
@@ -11,56 +11,58 @@ const questions = [
         name: "title",
         message: "What is the title of your project?",
     },
-    // {
-    //     type: "input",
-    //     name: "description",
-    //     message: "Enter a description of your project.",
-    // },
-    // {
-    //     type: "input",
-    //     name: "install",
-    //     message: "What are the installation instructions for your project?",
-    // },
-    // {
-    //     type: "input",
-    //     name: "contribute",
-    //     message: "How can others contribute to your project?",
-    // },
-    // {
-    //     type: "input",
-    //     name: "testing",
-    //     message: "What are the testing instructions for your project?",
-    // },
-    // {
-    //     type: "list",
-    //     name: "licence",
-    //     message: "What is the licence for your project?",
-    //     choices: ["MIT", "Clause", "BSD-2"],
-    // },
-    // {
-    //     type: "input",
-    //     name: "github",
-    //     message: "What is your GitHub username?",
-    // },
-    // {
-    //     type: "input",
-    //     name: "email",
-    //     message: "What is your email address?",
-    // },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project.",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "How does this project get used?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What are the installation instructions for your project?",
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "How can others contribute to your project?",
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "What are the testing instructions for your project?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What is the license for your project?",
+        choices: ["MIT", "Clause", "BSD-2", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, JSON.stringify(data, null, "\t"), (err) =>
-        err ? console.log(err) : console.log("Success!")
-    );
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((data) => {
-        generate.generateMarkdown(data);
-        writeToFile(`${data.title}`, data);
+        writeToFile("README.md", generate({ ...data }));
     });
 }
 
