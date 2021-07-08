b4df157456cb17aafaf7a7571fef4b16379d337b
Zathras <ChampionMike42@gmail.com>
2021-07-08 16:35:42 -0500

    Add code to all license functions and add license to generate function

---
+++ assets/js/utils/generateMarkdown.js
@@ -0,0 +1,81 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license !== "None") {
+        return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    }
+    return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (license === "MIT") {
+        return `<p>Copyright 2021 Mike Champion</p> 
+        <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+        <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+        <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>`;
+    } else if (license === "BSD-2") {
+        return `<p>Copyright 2021 Mike Champion</p> 
+        <p>Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:</p> 
+        <p>1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.</p> 
+        <p>2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.</p> 
+        <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p> `;
+    } else {
+        return "";
+    }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    // calls renderLicenseBadge and renderLicenseLink, returns built section
+    const badge = renderLicenseBadge(license);
+    const link = renderLicenseLink(license);
+    return `
+## <a id="license">VII. License</a>
+${badge}  
+
+${link}  
+`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Table of Contents
+I. [Description](#description)  
+II. [Install Instructions](#install-instructions)  
+III. [Usage Information](#usage-information)  
+IV. [Contribution Guidelines](#contribution-guidelines)  
+V. [Testing Instructions](#testing-instructions)  
+VI. [Contact](#contact)  
+VII. [License](#license)  
+
+## <a id="description">I. Description</a>
+${data.description}
+
+## <a id="install-instructions">II. Install Instructions</a>
+${data.install}
+## <a id="usage-information">III. Usage Information</a>
+${data.usage}
+### Project Screenshot
+![alt text](assets/images/screenshot.png)
+### Credits
+
+## <a id="contribution-guidelines">IV. Contribution Guidelines</a>
+${data.contribute}
+## <a id="testing-instructions">V. Testing Instructions</a>
+${data.testing}
+## <a id="contact">VI. Contact</a>
+### email
+${data.email}
+### GitHub
+${data.github}
+${renderLicenseSection(data.license)}
+`;
+}
+
+module.exports = generateMarkdown;
